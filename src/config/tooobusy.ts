da46de11d87bed5881d9e01da7e6104f3b94736c
jithin-kg <jithinjkg@gmail.com>
2021-01-16 11:04:13 +0530

    toobusy removed

--- src/config/tooobusy.ts
+++ src/config/tooobusy.ts
@@ -1,18 +1,18 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import * as toobusy from 'toobusy-js'
-import * as hpp from 'hpp';
-// https://cheatsheetseries.owasp.org/cheatsheets/Nodejs_Security_Cheat_Sheet.html
-@Injectable()
-export class TooBusyMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    hpp();//Prevent HTTP Parameter Pollution
-    if(toobusy()){
-        res.send("server Too busy")
-    }else{
-        next();
+// import { Injectable, NestMiddleware } from '@nestjs/common';
+// import { Request, Response, NextFunction } from 'express';
+// import * as toobusy from 'toobusy-js'
+// import * as hpp from 'hpp';
+// // https://cheatsheetseries.owasp.org/cheatsheets/Nodejs_Security_Cheat_Sheet.html
+// @Injectable()
+// export class TooBusyMiddleware implements NestMiddleware {
+//   use(req: Request, res: Response, next: NextFunction) {
+//     hpp();//Prevent HTTP Parameter Pollution
+//     if(toobusy()){
+//         res.send("server Too busy")
+//     }else{
+//         next();
 
-    }
-  }
-}
+//     }
+//   }
+// }
 
