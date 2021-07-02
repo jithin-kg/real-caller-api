3a21870d80b122d698fce4961243ac7796d8b7c4
Jithin KG <31859018+jithin-kg@users.noreply.github.com>
2021-07-02 10:10:23 +0530

    Revert "contacts/savecontacts api created-"

--- src/contactManage/myContacts/reqBodyDTO.ts
+++
@@ -1,8 +0,0 @@
-import { ArrayMaxSize, IsArray } from "class-validator";
-import { ReqContactDTO } from './reqContactDTO';
-
-export class ReqBodyDTO {
-    @IsArray()
-    @ArrayMaxSize(10000)
-    contacts: ReqContactDTO[];
-}
--- src/db/collection.names.ts
+++ src/db/collection.names.ts
@@ -1,6 +1,5 @@
-export class CollectionNames {
-    static CONTACTS_COLLECTION: string = "contactsNew";
-    static CONTACTS_OF_COLLECTION: string = "contactsOfUser";
-    static USERS_COLLECTION: string = "users";
-    static MY_CONTACTS: string = "myContacts";
+export class CollectionNames{
+    static CONTACTS_COLLECTION : string = "contactsNew";
+    static CONTACTS_OF_COLLECTION : string = "contactsOfUser";
+    static USERS_COLLECTION = "users"
 }
