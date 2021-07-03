fb6f65fcff671bf06ac812c3a948e4359294da4e
sarath <sarathptnr@gmail.com>
2021-07-03 11:27:31 +0530

    savemycontacts api bug fixed, split functions

---
+++ src/contactManage/myContacts/saveContactsHelper.ts
@@ -0,0 +1,40 @@
+import * as CryptoJS from 'crypto-js';
+import { ReqContactDTO } from './reqContactDTO';
+const SECRET_KEY: string = "___thisissecretkey___";
+export function do_AES_decryption(decryptedString: string): Promise<ReqContactDTO[]> {
+    return new Promise(async resolve => {
+        try {
+            var bytes = CryptoJS.AES.decrypt(decryptedString, SECRET_KEY);
+            let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8)) || [];
+            resolve(decryptedData);
+        } catch (error) {
+            console.log("decryotion error: ", error);
+            resolve(new Array());
+        }
+    });
+}
+export function do_AES_encryption(source: ReqContactDTO[]): Promise<string> {
+    return new Promise(async resolve => {
+        try {
+            const enc = CryptoJS.AES.encrypt(JSON.stringify(source), SECRET_KEY).toString()
+            resolve(enc);
+        } catch (error) {
+            console.log("encryption error: ", error);
+            resolve("");
+        }
+    });
+}
+export function findDifference(A: ReqContactDTO[], B: ReqContactDTO[]): Promise<ReqContactDTO[]> {
+    return new Promise(async resolve => {
+        console.log('A:', A)
+        console.log('B:', B)
+        const results = A.filter(({
+            hashedPhoneNumber: id1
+        }) => !B.some(({
+            hashedPhoneNumber: id2
+        }) =>
+            id2 === id1
+        ));
+        resolve(results)
+    })
+}
